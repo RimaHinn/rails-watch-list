99d27231ff31b509fff94e796a832487bf3a0b41
Rima Hinnawi <rima@restonyoga.com>
2023-02-28 01:08:46 +0000

    movie list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    root to: 'lists#index'
+    resources :lists, only: %i[index show new create edit update destroy] do
+      resources :bookmarks, only: %i[new create index]
+    end
+    resources :bookmarks, only: [:destroy]
+  end
 end
+
